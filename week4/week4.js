5d76bbbc521f70f4180df442599b2921240c6431
Hasan Omar <r3domar1@gmail.com>
2025-04-23 11:39:50 +0300

    added week4

---
+++ week4/week4.js
@@ -0,0 +1,47 @@
+// "use strict";
+
+// task 1
+// let name = "Mary ";
+// let activity = "drink tea";
+// let bio = "Our guest " + name + activity + ".";
+
+// console.log(bio.length);
+
+// const str =
+//   "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
+
+// task 2
+// let letter = prompt("Enter number to get letter");
+// console.log(str.charAt(+letter)); // str[letter]
+// console.log(typeof +letter);
+
+// task 3
+// let str2 = prompt("Enter string");
+// console.log(`last char : ${str2.at(-1)}`);
+// console.log(`before last char : ${str2.at(-2)}`);
+// console.log(`last char : ${str2[str2.length - 1]}`);
+// console.log(`before last char : ${str2[str2.length - 2]}`);
+
+// task 4
+
+// let a = "abcde";
+// let b = "";
+// for (let i = a.length - 1; i >= 0; i--) {
+//   b += a[i];
+// }
+// console.log(`original : ${a}`);
+// console.log(`reversed : ${b}`);
+
+// task 5
+
+// let js = "js";
+// console.log(js.toUpperCase());
+
+// task 6
+
+// const str =
+//   "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.";
+// let str2 = prompt("Enter string");
+// alert(str.includes(str2) ? "Your string is found !" : "String not found !");
+
+// task 7
